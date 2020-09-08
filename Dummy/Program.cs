5e891e88ed109caed6e7e60a0865ba1835e59e87
Nicholas Kier <nicholas.k.kier@gmail.com>
2020-09-08 14:54:07 -0400

    Second Commit

---
+++ Dummy/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dummy
+{
+    internal class Dummy
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name != null ? name : "NA";
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dummy d = new Dummy();
+            System.Console.WriteLine(d.Name);
+            d.Name = "Nick";
+            System.Console.WriteLine(d.Name);
+        }
+    }
+}
---
+++ TestEmployee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
